a97fa129ea139ebd4348d0b695a9a804fccaeb54
Matthew Hughes <matt.h17@yahoo.com>
2024-05-21 21:30:10 +0000

    Added state constructor

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
-    <div className={css.App}>
-      <Sidebar/>
+    <div className='App'>
+      <StatefulGreeting greeting="I'm a stateful class Component" name="matt" />
     </div>
   );
 }
