1b1e4ac2d264830a97589ba3497ceccc50edf094
Matthew Hughes <matt.h17@yahoo.com>
2024-05-26 12:56:23 +0000

    Added componet did mount to project

---
+++ my-app/src/components/Content.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import css from './css/Content.module.css'
+import { savedPosts } from '../posts.json'
+import PostItem from './PostItem'
+import Loader from './Loader'
+
+class Content extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoaded: false
+        }
+
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
+    }
+
+    
+    render() {
+        return (
+            <div className={css.Content}>
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                    <Loader/>
+                </div>
+                <div className={css.SearchResults}>
+                    <PostItem  savedPosts={savedPosts}/>
+                    : <Loader/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Content;
